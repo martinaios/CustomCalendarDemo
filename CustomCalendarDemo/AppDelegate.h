6f21e6cf4492d76b82117cafc7c2bf6fc48e01a5
Martina Makasare <martinam@xanadutec.com>
2019-03-25 12:16:39 +0530

    Initial Commit

---
+++ CustomCalendarDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomCalendarDemo
+//
+//  Created by Martina Makasare on 3/25/19.
+//  Copyright © 2019 Martina Makasare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomCalendarDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomCalendarDemo
+//
+//  Created by Martina Makasare on 3/25/19.
+//  Copyright © 2019 Martina Makasare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
